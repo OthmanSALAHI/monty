0ef37254f0dead5939957a3733e805cd6ca178e6
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-19 09:18:48 +0100

    strlen in str

--- stringfunc.c
+++ stringfunc.c
@@ -34,3 +34,21 @@ char *_strdup(char *str, FILE *fp)
 
 	return (new);
 }
+/**
+ * _strlen - Function that calculates a length of string
+ * @str: String to be checked
+ * Return: The length of string or -1 if it fails
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (-1);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	return (i);
+}
