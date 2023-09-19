dceeb36d6ebc70a5f8d96d1ddeca6d11c1fe1487
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-19 09:21:01 +0100

    the print string function

--- stringfunc.c
+++ stringfunc.c
@@ -52,3 +52,32 @@ int _strlen(char *str)
 
 	return (i);
 }
+/**
+ * pstr - Function that prints string of the stack
+ * @stack: stack structure
+ * @line_number: number of instruction
+ */
+void pstr(stack_t **stack, UN unsigned int line_number)
+{
+	stack_t *temp = NULL;
+
+	if (*stack == NULL)
+	{
+		putchar(10);
+		return;
+	}
+
+	temp = *stack;
+
+	while (temp)
+	{
+		if (temp->n <= 0 || temp->n >= 128)
+		{
+			putchar(10);
+			return;
+		}
+		printf("%c", temp->n);
+		temp = temp->next;
+	}
+	putchar(10);
+}
