0ec3db5fe451b332ccb6ec45f4c8e6c5504a0bfb
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-09-19 11:39:49 +0100

    extra

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+list_t *list_opcode = NULL;
+
+/**
+ * main - Main entry point for Stacks and Queues
+ * @argc: Argument count for the matrix
+ * @argv: Argument vector for the matrix
+ * Return: 0 on success or other number if it fails
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+	list_t *temp;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	get_file(argv[1]);
+
+	temp = list_opcode;
+
+	for (; temp; temp = temp->next)
+		(*ptr_opcode(temp))(&stack, temp->n);
+
+	free_all(list_opcode, stack);
+	return (0);
+}
